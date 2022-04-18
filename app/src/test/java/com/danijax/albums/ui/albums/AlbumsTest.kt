3d28defa277393ddaba7f216b039195b0d4e45e4
Daniel James <Daniel.james@wemabank.com>
2022-04-18 09:45:57 +0100

    Initial project setup
    Setup project with basic dependencies and tdd unit testing

--- app/src/test/java/com/danijax/albums/ui/albums/AlbumsTest.kt
+++ app/src/test/java/com/danijax/albums/ui/albums/AlbumsTest.kt
@@ -1,4 +1,20 @@
 package com.danijax.albums.ui.albums
 
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+
+
+@RunWith(MockitoJUnitRunner::class)
 class AlbumsTest {
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+
+    @Test
+    fun fetchAlbums(){
+
+    }
 }
